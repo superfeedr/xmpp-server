e630474ec0e0f7416cf83b51a679df29f96b1394
julien <julien.genestoux@gmail.com>
2011-09-09 18:26:41 +0200

    basic support for presence

--- modules/logger.js
+++ modules/logger.js
@@ -1,5 +1,5 @@
 var xmpp = require('node-xmpp');
-var winston = require('winston');
+var logger = require('winston');
 
 // http://xmpp.org/extensions/xep-0160.html
 exports.name = "logger";
@@ -8,33 +8,34 @@ exports.name = "logger";
 function Logger(client) {
     
     var self = this;
+    this.logger = logger;
     
     this.format_log = function (message) {
         return client.streamId + " : " + message
     }
     
     client.on('stanza', function(stanza) {
-        winston.debug(self.format_log(stanza.toString()));
+        logger.debug(self.format_log(stanza.toString()));
     });
     
     client.on('session-started', function() {
-        winston.info(self.format_log(stanza.toString()));
+        logger.info(self.format_log(stanza.toString()));
     });
 
     client.on('auth-success', function(jid) {
-        winston.info(self.format_log("auth-success " + jid));
+        logger.info(self.format_log("auth-success " + jid));
     });
 
     client.on('auth-failure', function(jid) {
-        winston.info(self.format_log("auth-failure " + jid));
+        logger.info(self.format_log("auth-failure " + jid));
     });
 
     client.on('registration-success', function(jid) {
-        winston.info(self.format_log("registration-success " + jid));
+        logger.info(self.format_log("registration-success " + jid));
     });
 
     client.on('registration-failure', function(jid) {
-        winston.info(self.format_log("registration-failure " + jid));
+        logger.info(self.format_log("registration-failure " + jid));
     });
 }
 
--- modules/presence.js
+++ modules/presence.js
@@ -4,6 +4,63 @@ var xmpp = require('node-xmpp');
 exports.name = "presence";
 
 function Presence(client) {
+    client.on('stanza', function(stanza) {
+        if (stanza.is('presence')) {
+            // Ok, now we must do things =)
+            if (!stanza.attrs.to && !stanza.attrs.type) {
+                // Section 5.1.1 in http://xmpp.org/rfcs/rfc3921.html#presence
+                if(!client.initial_presence_sent) {
+                    console.log("INITIAL PRESENCE FROM " + client.jid)
+                    client.initial_presence_sent = true;
+                    if(client.roster) {
+                        client.roster.subscriptions(["to", "both"], function(jids) {
+                            jids.forEach(function(jid) {
+                                stanza.attrs.type = "probe";
+                                stanza.attrs.to = jid;
+                                client.c2s.router.send(stanza); // TODO: Blocking Outbound Presence Notifications.
+                            });
+                        });
+                        client.roster.subscriptions(["from", "both"], function(jids) {
+                            jids.forEach(function(jid) {
+                                stanza.attrs.to = jid;
+                                client.c2s.router.send(stanza); // TODO: Blocking Outbound Presence Notifications.
+                            });
+                        });
+                    }
+                    // Send the presence to the other resources for this jid, if any.
+                    client.c2s.connectedClientsForJid(stanza.attrs.from).forEach(function(jid) {
+                        if(client.jid.resource != jid.resource) {
+                            stanza.attrs.to = jid.toString();
+                            console.log(stanza.toString());
+                            client.c2s.router.send(stanza); // TODO: Blocking Outbound Presence Notifications.
+                        }
+                    })
+                }
+                // Section 5.1.2 in http://xmpp.org/rfcs/rfc3921.html#presence
+                if((!stanza.attrs.type || stanza.attrs.type === "unavailable") && client.initial_presence_sent) {
+                    if(client.roster) {
+                        client.roster.subscriptions(["from", "both"], function(jids) {
+                            jids.forEach(function(jid) {
+                                stanza.attrs.to = jid;
+                                client.c2s.router.send(stanza); // TODO: Blocking Outbound Presence Notifications.
+                            });
+                        });
+                    }
+                }
+            }
+        }
+    });
+    
+    client.on('disconnect', function() {
+        // We need to send a <presence type="offline" > on his behalf
+        var stanza = new xmpp.Element('presence', {from: client.jid.toString(), type: "unavailable" });
+        client.c2s.connectedClientsForJid(client.jid.toString()).forEach(function(jid) {
+            if(client.jid.resource != jid.resource) {
+                stanza.attrs.to = jid.toString();
+                client.c2s.router.send(stanza); // TODO: Blocking Outbound Presence Notifications.
+            }
+        });
+    });
     
 }
 
