833f7ac8aeffc77a76a5e9bab9ca3e681293feba
julien <julien.genestoux@gmail.com>
2011-10-01 19:27:05 +0200

    separation of concerns and now using a message library for offline message storage

--- modules/offline.js
+++ modules/offline.js
@@ -1,31 +1,16 @@
 var xmpp = require('node-xmpp');
-var redis = require("redis").createClient();
+var message = require('../lib/message.js');
+var Message = message.Message;
 var ltx = require('ltx');
-
-redis.on("error", function (err) {
-    console.log("Redis connection error to " + redis.host + ":" + redis.port + " - " + err);
-});
-
-function deliverNextOfflineMessageforClient(client) {
-    redis.rpop(client.jid.bare().toString(), function(error, stanza) {
-        if(stanza) {
-            client.emit("outStanza", ltx.parse(stanza));
-            deliverNextOfflineMessageforClient(client);
-        }
-    });
-}
-
-function deliverOfflineMessagesForClient(client) {
-    deliverNextOfflineMessageforClient(client);
-}
-
 
 // http://xmpp.org/extensions/xep-0160.html
 exports.name = "offline";
 
 function RecipientOffline(client) {
     client.on("online", function() {
-        deliverOfflineMessagesForClient(client);
+        Message.for(client.jid.bare().toString(), function(message) {
+            client.emit("outStanza", ltx.parse(message.stanza));
+        });
     });
 }
 
@@ -33,9 +18,8 @@ exports.mod = RecipientOffline;
 exports.storeOfflineMessage = function(stanza) {
     if(stanza.is("message")) {
         stanza.c("delay", {xmlns: 'urn:xmpp:delay', from: '', stamp: ISODateString(new Date())}).t("Offline Storage");
-        jid = new xmpp.JID(stanza.attrs.to);
-        redis.lpush(jid.bare().toString(), stanza, function() {
-            redis.ltrim(jid.bare().toString(), 0, 9);
+        (new Message(new xmpp.JID(stanza.attrs.to).bare().toString(), stanza.toString())).save(function() {
+            // not much
         });
     }
 }
