2350808e6748ecc860f4388a89abef0e7472c3d5
julien <julien.genestoux@gmail.com>
2011-10-06 16:00:50 +0200

    adding the websocket module

---
+++ modules/websocket.js
@@ -0,0 +1,12 @@
+var websocket   = require('websocket').server;
+
+exports.name = "mod_websocket";
+
+function WebsocketServer() {
+}
+
+exports.mod = WebsocketServer;
+exports.configure = function(c2s, s2s) {
+    
+}
+
