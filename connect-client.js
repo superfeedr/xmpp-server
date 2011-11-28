2298fafd2a8ee61793bd587090366f26a5ddc1a4
julien <julien.genestoux@gmail.com>
2011-11-28 14:10:52 +0100

    adding a functionnal test

---
+++ connect-client.js
@@ -0,0 +1,21 @@
+/**
+ * This attempts to connect to the XMPP server.
+ **/
+var sys = require('sys');
+var xmpp = require('node-xmpp');
+var argv = process.argv;
+
+if (argv.length != 4) {
+    sys.puts('Usage: node connect-client.js <my-jid> <my-password>');
+    process.exit(1);
+}
+
+var cl = new xmpp.Client({
+    jid: argv[2],
+    password: argv[3]
+});
+
+cl.on('online', function () {
+    cl.send(new xmpp.Element('presence', {}).c('show').t('chat').up().c('status').t('If I\'m online, then, I\'m fine!'));
+    sys.puts('You win');
+});
