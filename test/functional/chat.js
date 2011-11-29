99b5edb8ab33e03f9249f33398fd7062d877c618
julien <julien.genestoux@gmail.com>
2011-11-29 14:31:05 +0100

    using the mocha syntax for the chat example

--- test/functional/chat.js
+++ test/functional/chat.js
@@ -9,69 +9,73 @@ var User = require('../../lib/users.js').User;
 fixtures = [["bernard@localhost", "bErnArD"], ["bianca@localhost", "B1anCA"]]; // Fixtures 
 
 
-var done = _.after(fixtures.length, function() {
-    // Connects Bernard
-    var bernard = new xmpp.Client({
-        jid: "bernard@localhost",
-        password: "bErnArD"
+
+describe('A small chat', function(){
+    var bernard, bianca = null;
+
+    beforeEach(function(proceed){
+        var ready = _.after(fixtures.length, function() {
+            bernard = new xmpp.Client({
+                jid: "bernard@localhost",
+                password: "bErnArD"
+            });
+            bianca = new xmpp.Client({
+                jid: "bianca@localhost",
+                password: "B1anCA"
+            });
+
+            // When Bernard is online. Let's get him to reply to Bianca only if she says 'Hello Bernard!'. If she doesn't, then, we fail.
+            bernard.on('online', function () {
+                bernard.send(new xmpp.Element('presence', {}).c('show').t('chat').up().c('status').t('Bernard is here to help!'));
+                online();
+            });
+
+            bianca.on('online', function () {
+                bianca.send(new xmpp.Element('presence', {}).c('show').t('chat').up().c('status').t('Ready to help!'));
+                online();
+            });
+            var online = _.after(2, function() {
+                proceed();
+            });
+        });
+        _.each(fixtures, function(fixture) {
+            User.register(fixture[0], fixture[1], {
+                force: true,
+                success: ready,
+                error: function(err) {
+                    console.log("Couldn't add users.");
+                    process.exit(1);
+                }
+            });
+        });
     });
 
-    // Connects Bianca
-    var bianca = new xmpp.Client({
-        jid: "bianca@localhost",
-        password: "B1anCA"
-    });
-    
-    var online = _.after(2, function() {
-        // When they're both online
+    it('bernard should get bianca\'s messages', function(done){
+        var biancaSays = 'Hello Bernard!';
         bernard.on('stanza', function(stanza) {
-            if (stanza.is('message') && stanza.attrs.type !== 'error' && stanza.getChild('body').getText() == "Hello Bernard!") {
-                console.log("Bianca said hello!");
-                bianca.send(new xmpp.Element('message', {to: stanza.attrs.from}).c('body').t('Hi Bianca!'));
+            if (stanza.is('message') && new xmpp.JID(stanza.attrs.from).bare().toString() == "bianca@localhost" && stanza.getChild('body').getText() == biancaSays) {
+                done();
             }
             else {
-                console.log("Bianca didn't say hello!");
-                process.exit(1);
+                throw function() {}; // Bernard didn't get Bianca's Hello.
             }
         });
-        
+        bianca.send(new xmpp.Element('message', {to: "bernard@localhost"}).c('body').t(biancaSays));
+    });
+    
+    it('bianca should get bernard\'s messages', function(done){
+        var bernardSays = 'Hi Bianca!';
         bianca.on('stanza', function(stanza) {
-            if (stanza.is('message') && stanza.attrs.type !== 'error' && stanza.getChild('body').getText() == "Hi Bianca!") {
-                console.log("Bernard said hi.");
-                process.exit(0);
+            if (stanza.is('message') && new xmpp.JID(stanza.attrs.from).bare().toString() == "bernard@localhost" && stanza.getChild('body').getText() == bernardSays) {
+                done();
             }
             else {
-                console.log("Bernard didn't say hi.");
-                process.exit(1);
+                throw function() {}; // Bernard didn't get Bianca's Hello.
             }
         });
-        
-        bianca.send(new xmpp.Element('message', {to: "bernard@localhost"}).c('body').t('Hello Bernard!'));
+        bernard.send(new xmpp.Element('message', {to: "bianca@localhost"}).c('body').t(bernardSays));
     });
-    
-    
-    // When Bernard is online. Let's get him to reply to Bianca only if she says 'Hello Bernard!'. If she doesn't, then, we fail.
-    bernard.on('online', function () {
-        bernard.send(new xmpp.Element('presence', {}).c('show').t('chat').up().c('status').t('Bernard is here to help!'));
-        online();
-    });
-    
-    bianca.on('online', function () {
-        bianca.send(new xmpp.Element('presence', {}).c('show').t('chat').up().c('status').t('Ready to help!'));
-        online();
-    });
+
 });
-/*
-Prep : user registration
-*/
-_.each(fixtures, function(fixture) {
-    User.register(fixture[0], fixture[1], {
-        force: true,
-        success: done,
-        error: function(err) {
-            console.log("Couldn't add users.");
-            process.exit(1);
-        }
-    })
-});
+
 
