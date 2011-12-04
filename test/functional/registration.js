e8ae0b022b95d14a4e13c703cd6f1b72ca34a133
julien <julien.genestoux@gmail.com>
2011-12-04 14:53:41 +0100

    adding a mocha test for registration

---
+++ test/functional/registration.js
@@ -0,0 +1,33 @@
+/**
+* This attempts to connect to the XMPP server.
+**/
+var xmpp = require('node-xmpp');
+var server  = require('../../lib/server.js');
+
+var _ = require('underscore');
+var User = require('../../lib/users.js').User;
+
+
+describe('Register a new jid', function(){
+    
+    before(function(proceed) {
+        server.run({port: 5222, domain: 'localhost', bindAddress: '0.0.0.0'}, function() {
+            proceed();
+        });
+    });
+    
+    beforeEach(function(proceed){
+        proceed();
+    });
+    
+    it('should allow for user registration for a new jid', function(done){
+        // Can't be implemented as it seems that node-xmpp doesn't support registration for now :(
+    });
+
+    it('should prevent duplication if that jid exists', function(done){
+        // Can't be implemented as it seems that node-xmpp doesn't support registration for now :(
+    });
+    
+});
+
+
