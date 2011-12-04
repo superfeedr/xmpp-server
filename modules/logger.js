dbacc484bc5a220b64c91bae557c170942a35760
julien <julien.genestoux@gmail.com>
2011-12-04 15:55:08 +0100

    now logging only if there is a logger config

--- modules/logger.js
+++ modules/logger.js
@@ -16,32 +16,33 @@ exports.configure = function(server, config) {
     // Config contains the configuration for the logger facility!
     // The logger relies purely on events... 
     // From there, we can access and listen to events on all objects linked to that server, including the router, the session manager, the S2S router, the connections... etc.
+    if(config) {
+        server.on("connect", function(client) {
+            logger.debug(format_log(client, "connected"));
 
-    server.on("connect", function(client) {
-        logger.debug(format_log(client, "connected"));
+            client.on('session-started', function() {
+                logger.info(format_log(client, stanza.toString()));
+            });
 
-        client.on('session-started', function() {
-            logger.info(format_log(client, stanza.toString()));
+            client.on('auth-success', function(jid) {
+                logger.info(format_log(client, "auth-success " + jid));
+            });
+
+            client.on('online', function() {
+                logger.info(format_log(client, "online " + client.jid));
+            });
+
+            client.on('auth-failure', function(jid) {
+                logger.info(format_log(client, "auth-failure " + jid));
+            });
+
+            client.on('registration-success', function(jid) {
+                logger.info(format_log(client, "registration-success " + jid));
+            });
+
+            client.on('registration-failure', function(jid) {
+                logger.info(format_log(client, "registration-failure " + jid));
+            });
         });
-
-        client.on('auth-success', function(jid) {
-            logger.info(format_log(client, "auth-success " + jid));
-        });
-
-        client.on('online', function() {
-            logger.info(format_log(client, "online " + client.jid));
-        });
-
-        client.on('auth-failure', function(jid) {
-            logger.info(format_log(client, "auth-failure " + jid));
-        });
-
-        client.on('registration-success', function(jid) {
-            logger.info(format_log(client, "registration-success " + jid));
-        });
-
-        client.on('registration-failure', function(jid) {
-            logger.info(format_log(client, "registration-failure " + jid));
-        });
-    });
+    }
 }
